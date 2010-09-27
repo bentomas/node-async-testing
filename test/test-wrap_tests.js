508aeb5f8a54e43439e1b7769ecbb9acc7bbc7d2
Benjamin Thomas <benjamin@benjaminthomas.org>
2010-09-26 21:09:23 -0600

    Update wrap tests to hande sub suites (and use them!)

--- test/test-wrap_tests.js
+++ test/test-wrap_tests.js
@@ -5,28 +5,72 @@ var async_testing = require('../lib/async_testing')
 var extra1 = {}, extra2 = {};
 
 module.exports = {
-  'test wrapped': function(test) {
-    test.numAssertions = 4;
-    test.strictEqual(test.one, extra1);
-    test.strictEqual(test.two, extra2);
-    test.finish();
-  }
+  'sync wrap': wrap(
+    { suite:
+      { 'test': function(test) {
+          test.numAssertions = 5;
+          test.strictEqual(test.one, extra1);
+          test.strictEqual(test.two, extra2);
+          test.finish();
+        }
+      }
+    , setup: function setup(test, done) {
+        test.ok(true, 'make sure we run the setup');
+        test.one = extra1;
+        test.two = extra2;
+    
+        done();
+      }
+    , teardown: function setup(test, done) {
+        test.ok(true, 'make sure we run the teardown');
+        test.one = extra1;
+        test.two = extra2;
+    
+        done();
+      }
+    }),
+
+  'async setup': wrap(
+    { suite:
+      { 'test': function(test) {
+          test.numAssertions = 4;
+          test.strictEqual(test.one, extra1);
+          test.strictEqual(test.two, extra2);
+          test.finish();
+        }
+      }
+    , setup: function setup(test, done) {
+        test.ok(true, 'make sure we run the setup');
+        test.one = extra1;
+        test.two = extra2;
+    
+        setTimeout(done, 500);
+      }
+    }),
+
+  'async teardown': wrap(
+    { suite:
+      { 'test': function(test) {
+          test.numAssertions = 2;
+          test.finish();
+        }
+      }
+    , teardown: function setup(test, done) {
+        test.ok(true, 'make sure we run the teardown');
+    
+        setTimeout(done, 500);
+      }
+    }),
 };
 
-wrap({ suite: module.exports
-     , setup: function setup(test, done) {
-         test.ok(true, 'make sure we run the setup');
-         test.one = extra1;
-         test.two = extra2;
+wrap( { suite: module.exports
+      , setup: function(test, done) {
+          test.ok(true, 'make sure we get to outer level setup')
 
-         done();
-       }
-     , teardown: function teardown(test, done) {
-         test.ok(true, 'make sure we run the teardown');
+          done();
+        }
+      });
 
-         done();
-       }
-     });
 
 if (module == require.main) {
   async_testing.run(__filename, process.ARGV);
