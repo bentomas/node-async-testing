8bbb288eaf3fca6022a7cd6a6a4260cb63a7050a
Benjamin Thomas <benjamin@benjaminthomas.org>
2010-10-07 19:55:41 -0600

    Restructer how tests are ran
    This change cleans up the test running code quite a bit.  Now (more
    logically) whenever a test finishes, whether by assertion failure, error
    or a `test.finish()` call, it all goes through the `testFinished`
    function.  Before errors were handled differently.
    
    We also simplify the event API for writing test runners.  Before we had
    4 different kind of result objects, now we have 2 (mostly).  I know this
    is a backwards incompatible change, but it isn't to the public API, but
    to the test runner API which I think I am the only one doing.  So, if
    this is breaking your code, please let me know.
    
    We restructred as a preface to proper setup/teardown support, which hopefully
    I'll get to tonight.

--- test/test-multiple_errors.js
+++ test/test-multiple_errors.js
@@ -2,23 +2,23 @@
 module.exports = {
   'test async error 1': function(test) {
     process.nextTick(function() {
-        throw new Error();
+        throw new Error('error 1');
       });
   },
 
   'test sync error': function(test) {
-    throw new Error('Oooops');
+    throw new Error('sync error');
   },
 
   'test async error 2': function(test) {
     setTimeout(function() {
-        throw new Error();
+        throw new Error('error 2');
       }, 500);
   },
 
   'test async error 3': function(test) {
     setTimeout(function() {
-        throw new Error();
+        throw new Error('error 3');
       }, 500);
   }
 };
