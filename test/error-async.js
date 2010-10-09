e5090414e469636702d2a7617c418726e0c58555
Benjamin Thomas <benjamin@benjaminthomas.org>
2010-10-09 14:35:28 -0600

    Make unexpected errors kill the process
    I realized I was going to A LOT of effort to catch and report on
    run-time errors in code, and this was majorly complicating the way the
    whole library was being written.
    
    But I figure if there is an error, it is something that is a show
    stopper anyway, and you need to be notified about it immediately.
    
    Suite runners will be notified of the error, right before the process
    exits, so they can say what happened.
    
    If you still really want errors to be caught, use the wrap function to
    add a setup to all your tests and then set the uncaughtExceptionHandler
    in the test.  You'll have to run the suite in parallel then.

--- test/error-async.js
+++ test/error-async.js
@@ -1,27 +1,17 @@
 
 module.exports = {
   'test async error 1': function(test) {
-    process.nextTick(function() {
+    setTimeout(function() {
         throw new Error('error 1');
-      });
-  },
-
-  'test sync error': function(test) {
-    throw new Error('sync error');
+      }, 500);
   },
 
   'test async error 2': function(test) {
     setTimeout(function() {
         throw new Error('error 2');
       }, 500);
-  },
-
-  'test async error 3': function(test) {
-    setTimeout(function() {
-        throw new Error('error 3');
-      }, 500);
   }
-};
+}
 
 if (module == require.main) {
   require('../lib/async_testing').run(__filename, process.ARGV);
