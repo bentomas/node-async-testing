a621b489935dfe580eaeaf3e8b368708a8e49864
Dominic Tarr <dominic.tarr@gmail.com>
2010-11-08 19:33:43 +1300

    updated testing

--- test/test-error_test_already_finished_then_assertion.js
+++ test/test-error_test_already_finished_then_assertion.js
@@ -1,11 +1,10 @@
+
 if (module == require.main) {
   return require('../lib/async_testing').run(process.ARGV);
 }
-
 module.exports = {
-  'test sync already finished': function(test) {
+  'test sync already finished then assertion': function(test) {
     test.finish();
-    test.finish();
+    test.ok(true,'this should fail');
   },
 }
-
