512bb9b4f2bf4a95b7ba60f6a6567c9418cebbf7
Stephen Judkins <stephen.judkins@gmail.com>
2010-12-10 15:35:08 -0800

    Fixed issue #6.  One can now throw a non-Error object without messing up the entire suite.

--- test/test-errors.js
+++ test/test-errors.js
@@ -9,6 +9,10 @@ exports['test async error'] = function(test) {
     }, 500);
 };
 
+exports['test a thrown non-Error object'] = function(test) {
+  throw "a string, not an error";
+}
+
 if (module == require.main) {
   require('../lib/async_testing').run(__filename, process.ARGV);
 }
