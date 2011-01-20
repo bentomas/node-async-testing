d3b854cecf8a9277081518b3c5c7d12fa7eee559
Dominic Tarr <dominic.tarr@gmail.com>
2011-01-20 16:44:58 +1300

    added example to show weird errors and then fixed it.

--- test/test-weird_throws.js
+++ test/test-weird_throws.js
@@ -18,4 +18,17 @@ module.exports = {
 , 'test throw String': function(test) {
     throw 'hello';
   }
+/*, 'test throw error without stack': function(test) {
+    //weird but it happens some times... stackoverflow, for example.
+    var e = new Error ("THIS ERROR HAS NO STACK TRACE")
+    delete e.stack
+    throw e   
+  }*/
+, 'test stack overflow': function(test) {
+    //weird but it happens some times... stackoverflow, for example.
+    function overflow(){
+      overflow()
+    }
+    overflow()
+  }
 }
